8c4d2489d7762945cf2139871e0d221d1714971d
Vinayak <vinayakdeepu@gmail.com>
2021-09-24 14:35:01 +0530

    Add audio mute button

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,10 +1,12 @@
+import { state } from "./config";
+
 const bgm = new Audio("audio/ahsoka-theme.mp3");
+const bgmVolume = 0.3;
 const saberHum = new Audio("audio/saber-hum.mp3");
 const saberIgnition = new Audio("audio/saber-ignition.mp3");
 
 export const playBgm = () => {
   bgm.loop = true;
-  bgm.volume = 0.5;
   bgm.play();
 };
 
@@ -24,3 +26,25 @@ export const stopSaberHum = () => {
 export const playSaberIgnition = () => {
   saberIgnition.play(); // no loop
 };
+
+
+const muteBtn = $("#mute-btn");
+const updateVolume = () => {
+  if (state.bgmMuted) {
+    bgm.volume = 0;
+    saberHum.volume = 0;
+    saberIgnition.volume = 0;
+    muteBtn.html(`<em class="fas fa-volume-mute"></em>`)
+  } else {
+    bgm.volume = bgmVolume;
+    saberHum.volume = 1;
+    saberIgnition.volume = 1;
+    muteBtn.html(`<em class="fas fa-volume-up"></em>`)
+  }
+}
+
+updateVolume();
+muteBtn.click(() => {
+  state.bgmMuted = !state.bgmMuted;
+  updateVolume();
+});
